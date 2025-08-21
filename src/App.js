b9941d62513a6cb3c15098f42071f1dddb743cbb
wendyleseno18 <wendyle.seno@yotpo.com>
2025-08-22 00:15:03 +0800

    Update product id

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "115798";
+  let ProductId = "114916";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
