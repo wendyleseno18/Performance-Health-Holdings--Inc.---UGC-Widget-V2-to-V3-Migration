606e1d887cc48252a8500f5224f04457ef934f09
wendyleseno18 <wendyle.seno@yotpo.com>
2025-08-22 00:13:44 +0800

    App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "128405";
+  let ProductId = "115798";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
